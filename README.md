dd4a6946d007d1e54a6761c347f8996328b0a64d
unknown <dharano@users.noreply.github.com>
2021-03-22 11:26:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# usetestfetch
+ 
---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+interface Book {
+    name: string
+}
+
+const App = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  const fetchBooks = async () => {
+      const response: Response = await fetch(
+          'https://www.anapioficeandfire.com/api/books',
+          {headers: {'Content-Type': 'application/json'}, method: "GET"}
+      );
+      setBooks(await response.json());
+  };
+
+  return (
+    <div className="App">
+   <button onClick={fetchBooks}>Get GoT books</button>
+   {books.map((book, index) => {
+       const indexToDisplay = index += 1;
+       return <div key={`book${index}`}>{indexToDisplay}&nbsp;{book.name}</div>
+   })}
+</div>
+  );
+};
+
+export default App;
